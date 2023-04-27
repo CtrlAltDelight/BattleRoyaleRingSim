25a3a4f497ca3c10e6008561283c76ae657b6e56
CtrlAltDelight <lbchigges@gmail.com>
2023-04-27 14:44:52 -0400

    Added skeleton file

---
+++ graphics.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <assert.h>
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_timer.h>
+
+int main(int argc, char* argv[]) {
+	return EXIT_SUCCESS;
+}
+/* vim: set tabstop=4 shiftwidth=4 fileencoding=utf-8 noexpandtab: */
